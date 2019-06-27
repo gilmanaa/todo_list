ed33665412424a3d3f10e10dc8c0cf8c0c10796d
Aaron Gilman <gilmanaa2@gmail.com>
2019-06-27 23:38:52 +0300

    js functionality for delete and move completed

--- js/logic.js
+++ js/logic.js
@@ -1,19 +1,19 @@
 class Task extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.deleteTask = this.deleteTask.bind(this);
         this.moveTask = this.moveTask.bind(this);
     }
-    deleteTask(){
-        this.props.delete(this.props.arrPosition);
+    deleteTask() {
+        this.props.delete(this.props.arrPosition, this.props.design);
     }
-    moveTask(){
-        this.props.move(this.props.arrPosition);
+    moveTask() {
+        this.props.move(this.props.arrPosition, this.props.design);
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className={this.props.design}>
-                <input type="checkbox" onClick={this.moveTask}/>
+                <input type="checkbox" onClick={this.moveTask} />
                 {this.props.text}
                 <button className="delete" onClick={this.deleteTask}>Delete</button>
             </div>
@@ -21,7 +21,7 @@ class Task extends React.Component {
     }
 }
 class ToDo extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             input: "",
@@ -34,43 +34,50 @@ class ToDo extends React.Component {
         this.adds = 0;
         this.addTask = this.addTask.bind(this);
         this.deleteTask = this.deleteTask.bind(this);
-        this.moveTaskDone = this.moveTaskDone.bind(this);
-        this.moveTaskRedo = this.moveTaskRedo.bind(this);
+        this.moveTask = this.moveTask.bind(this);
     }
-    moveTaskRedo(taskNumber) {
-        var taskPosition = this.done.findIndex(i => i.key === taskNumber);
-        var moveTask = this.done.splice(taskPosition,1);
-        console.log(moveTask);
-        
-        this.setState({
-            add: true,
-            completeTask: false,
-            key: moveTask[0].key
-        })
-        
+    moveTask(taskNumber, category) {
+        if (category === "done") {
+            var taskPosition = this.done.findIndex(i => i.key === taskNumber);
+            var moveTask = this.done.splice(taskPosition, 1);
+            this.setState({
+                add: true,
+                completeTask: false,
+                input: moveTask[0].props.text,
+                key: moveTask[0].key
+            })
+        } else {
+            var taskPosition = this.todo.findIndex(i => i.key === taskNumber);
+            var moveTask = this.todo.splice(taskPosition, 1);
+            this.setState({
+                add: false,
+                completeTask: true,
+                input: moveTask[0].props.text,
+                key: moveTask[0].props.arrPosition
+            })
+        }
     }
-    moveTaskDone(taskNumber){
-        var taskPosition = this.todo.findIndex(i => i.key === taskNumber);
-        var moveTask = this.todo.splice(taskPosition,1);
-        this.setState({
-            add: false,
-            completeTask: true,
-            input: moveTask[0].props.text,
-            key: moveTask[0].props.arrPosition
-        })
-        console.log(this.state.key)
+    deleteTask(taskNumber, category) {
+        if (category === "todo") {
+            var taskPosition = this.todo.findIndex(i => i.key === taskNumber);
+            this.todo.splice(taskPosition, 1);
+            this.setState({
+                add: false,
+                completeTask: false
+            })
+        } else {
+            var taskPosition = this.done.findIndex(i => i.key === taskNumber);
+            this.done.splice(taskPosition, 1);
+            this.setState({
+                add: false,
+                completeTask: false
+            })
+        }
+
     }
-    deleteTask(taskNumber){
-        var taskPosition = this.todo.findIndex(i => i.key === taskNumber);
-        this.todo.splice(taskPosition,1);
-        this.setState({
-            add: false,
-            completeTask: false
-        })
-    }
-    addTask(){
+    addTask() {
         this.adds++;
-        this.setState({ 
+        this.setState({
             add: true,
             completeTask: false,
             input: this.input.value,
@@ -78,24 +85,26 @@ class ToDo extends React.Component {
         });
         $(".add-task-input").val("");
     }
-    render(){
+    render() {
         if (this.state.add) {
-            this.todo.push(<Task design="todo" text={this.state.input} delete={this.deleteTask} move={this.moveTaskDone} arrPosition={this.state.key} key={this.state.key}/>)
+            this.todo.push(<Task design="todo" text={this.state.input} delete={this.deleteTask} move={this.moveTask} arrPosition={this.state.key} key={this.state.key} />)
         }
         if (this.state.completeTask) {
-            this.done.push(<Task design="done" text={this.state.input} delete={this.deleteTask} move={this.moveTaskRedo} arrPosition={this.state.key} key={this.state.key}/>)
+            this.done.push(<Task design="done" text={this.state.input} delete={this.deleteTask} move={this.moveTask} arrPosition={this.state.key} key={this.state.key} />)
         }
-        return(
+        var todoHeader = this.todo.length > 0 ? "Tasks/Goals" : null;
+        var doneHeader = this.done.length > 0 ? "Accomplishments" : null;
+        return (
             <div>
                 <div className="input-wrapper">
-                    <input className="add-task-input" type="text" ref={x => this.input = x} placeholder="Greatness awaits outside the comfort zone..."/>
+                    <input className="add-task-input" type="text" ref={x => this.input = x} placeholder="Greatness awaits outside the comfort zone..." />
                     <button className="add-task" onClick={this.addTask}>+</button>
                 </div>
-                <div className="to-do-start">Tasks/Goals</div>
+                <div className="to-do-start">{todoHeader}</div>
                 <div>
                     {this.todo}
                 </div>
-                <div className="to-do-start">Accomplishments</div>
+                <div className="to-do-start">{doneHeader}</div>
                 <div id="doneContainer">
                     {this.done}
                 </div>
@@ -104,11 +113,11 @@ class ToDo extends React.Component {
     }
 }
 class Header extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
     }
-    render(){
-        return(
+    render() {
+        return (
             <div className="header">
                 <div >{this.props.text}</div>
                 <div className={this.props.class}></div>
@@ -117,10 +126,10 @@ class Header extends React.Component {
     }
 }
 class App extends React.Component {
-    render(){
-        return(
+    render() {
+        return (
             <div className="app-wrapper">
-                <Header text="Comfort Zone" class="logo"/>
+                <Header text="Comfort Zone" class="logo" />
                 <ToDo />
             </div>
         )
