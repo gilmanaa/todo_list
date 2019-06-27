227c8362a0224e6cb532bd872e0d0abdafa0f994
Aaron Gilman <gilmanaa2@gmail.com>
2019-06-27 14:08:52 +0300

    added the header

--- js/logic.js
+++ js/logic.js
@@ -1,4 +1,27 @@
-
+class Header extends React.Component {
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div className="header">
+                <div >{this.props.text}</div>
+                <div className={this.props.class}></div>
+            </div>
+        )
+    }
+}
+class App extends React.Component {
+    render(){
+        return(
+            <div className="app-wrapper">
+                <Header text="Comfort Zone" class="logo"/>
+                {/* <ToDo/>
+                <Done/> */}
+            </div>
+        )
+    }
+}
 function render() {
     ReactDOM.render(
         <App />,
